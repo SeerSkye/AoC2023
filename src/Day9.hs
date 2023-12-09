df4480de883349661563fe98f3ba9baed00e87f5
SeerSkye <58483868+SeerSkye@users.noreply.github.com>
2023-12-09 15:42:17 -0500

    for fun, added a second solution

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,4 +1,6 @@
 module Main where
+import Data.Ratio
+import Data.List(unfoldr)
 
 readInput :: IO [[Int]]
 readInput = fmap (fmap read . words) . lines <$> readFile "input/day9.txt"
@@ -28,3 +30,37 @@ main = do
     input <- readInput
     putStrLn $ "Part 1 Solution: " ++ show (part1 input)
     putStrLn $ "Part 2 Solution: " ++ show (part2 input)
+
+-- For fun, let's code an alternate solution where we find closed-form equations
+-- for the formulas. We'll do this by "integrating" the constant function we eventually
+-- into a polynomial written in terms of falling factorial powers instead of normal powers
+-- (because normal powers don't play nice with the calculus of finite differences)
+
+-- The falling factorial power x .^ n is equal to x(x-1)...(x-n+1)
+(.^) :: Num a => a -> Int -> a
+_ .^ 0 = 1
+x .^ n = product $ take n $ iterate (\d -> d - 1) x
+
+-- Transform a list of coefficients of a polynomial of falling powers into a new
+-- list for the anti-difference: the function whos differences are the function passed
+-- in and who's value at f(0) is the value passed in
+antiDifference :: Integral a => Ratio a -> [Ratio a] -> [Ratio a]
+antiDifference c coefficients = c : zipWith (/) coefficients [1..]
+
+evaluateFromCoefficients :: Integral a => [Ratio a] -> Ratio a -> Ratio a
+evaluateFromCoefficients coefficients x = sum $ fmap (\(c, n) -> c * (x .^ n)) (zip coefficients [0..])
+
+findCoefficientsForSequence :: [Int] -> [Ratio Int]
+findCoefficientsForSequence s =
+    let differences = unfoldr (\diff -> if all (== 0) diff 
+                                         then Nothing 
+                                         else Just (diff, zipWith (-) (drop 1 diff) diff)) s 
+    in foldr antiDifference [] $ fmap (fromIntegral . head) differences
+
+part1' :: [[Int]] -> Ratio Int
+part1' = sum . fmap (\s -> evaluateFromCoefficients 
+                               (findCoefficientsForSequence s) 
+                               (fromIntegral $ length s))
+
+part2' :: [[Int]] -> Ratio Int
+part2' = sum . fmap (\s -> evaluateFromCoefficients (findCoefficientsForSequence s) (-1))
